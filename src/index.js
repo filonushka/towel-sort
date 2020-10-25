aebc52213f929b99eecfc256693f516b645ba267
filonushka <lero4ka.filonova@mail.ru>
2020-10-26 00:32:59 +0300

    refactor; finish task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function towelSort(matrix) {
     let towelSorted = [];
-    for (i = 0, i < matrix.length, i++) {
+    for (i = 0; i < matrix.length; i++) {
         if (i % 2 === 0) {
             towelSorted = towelSorted.concat(matrix[i])
         } else {
             towelSorted = towelSorted.concat(matrix[i].reverse());
         }
-    }
-    return towelSorted;
+
+    } return towelSorted;
 }
